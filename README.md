f3ebdd9ada3b15618279d45922fe3067b8495863
Eric Woolard <premierproductions601@gmail.com>
2017-02-28 08:28:51 -0600

    Updated to use incoming webhook integration

--- README.md
+++ README.md
@@ -1,22 +1,27 @@
 REQUIREMENTS
 ------------
-1. curl
-2. Python 3.5
-3. PRAW
-4. Slack API token for bot
+1. Slack Incoming Webhook Integration (https://api.slack.com/incoming-webhooks)
+2. Python 3+
+3. PRAW 4+
+4. Reddit API Token & Secret for the script
 
 How to use
 ----------
-1. Place Q-Killer script in a new directory (~/qkiller/ will do)
-2. Edit credentials on line 10 in qkiller.py
-3. Edit subreddit on line 19
-4. Adjust alert param number on lines 30-31 (optional)
-5. Edit Slack API info lines 35-37
-6. start the script with `nohup` by running `nohup python3 qkiller.py &` 
+1. Place Q-Killer script in a new directory
+2. Edit lines 11-15 with your credentials, and line 16 with your subreddit
+3. Edit line 47 with your Slack incoming webhook URL 
+4. *OPTIONAL*: Adjust alert parameter on line 39 (`if count >= 15:`). This decides how many things in queue it takes to trigger the alert
+5. Start the script with `nohup` by running `nohup python3 qkiller.py &` (Linux)
 
-If you only have 1 version of python installed, just use `python` instead of `python3`
+If you only have 1 version of python installed, just use `python` instead of `python3` when starting the script.
 
 INFO
 ----------
 This script was made for the /r/GlobalOffensive mod team, to alert us in Slack if the modqueue reaches 15 or more items.
 Use `nohup` to run in the background and ignore the hangup signal. (See 'How to use' above)
+
+To obtain a client ID and client secret, login to the Reddit account that will be using the script, and go to preferences>apps.
+Click on "Create a new app" (may say "Create another app") and select the 'Script' type. Name/description can be whatever. You can
+leave the about URL blank, but you still must provide a redirect URI even though it's not needed for a script application. For 
+that, you can just put `http://localhost/`. The string at the top left (underneath what should say personal use script) will be
+your client_ID. Client secret should be obvious :P
--- qkiller.py
+++ qkiller.py
@@ -1,49 +1,71 @@
 import praw
 import os
 import time
-import datetime
+from datetime import datetime
 import traceback
+import requests
 
-r = praw.Reddit(user_agent='r/subreddit ModQ counter script')
 
-try:
-    r.login('USERNAME', 'PASSWORD', disable_warning=True)
-except:
-    error_message = traceback.format_exc()
-    now = str(datetime.datetime.now())
-    print(now + ' - Error:\n' + error_message + '\n\n\n')
-    time.sleep(300)
-
-while True:
+def setup():
     try:
-        subreddit = r.get_subreddit('SUBREDDIT')
-        startTime = time.time()
-
-        print('Gathering items in queue...')
-        modQueue = subreddit.get_mod_queue()
-        count = 0
-
-        for item in modQueue:
-            count += 1
-        print('Total items in queue: ' + str(count))
-
-        if count >= 15:
-            print('Items in queue higher than 15! Alerting Slack.')
-            message = '<!here> *Uhh..HELLO?? What are you doing?? THERE ARE {} THINGS IN MODQUEUE...GET TO WORK!*'.format(
-                str(count))
-            cmd = 'curl https://slack.com/api/chat.postMessage -X POST -d "channel=#CHANNELNAME" -d "text=' \
-                  + message + '" -d "username=USERNAME" -d ' \
-                              '"token=SLACK_TOKEN_HERE" -d ' \
-                              '"icon_emoji=:ICON_NAME:"'
-            os.system(cmd)
-
-        elapsedTime = str(round(time.time() - startTime, 2))
-        finish = 'Cycle completed in {}s. Waiting for next interval...'.format(elapsedTime)
-        print(finish)
-        time.sleep(180)
+        reddit = praw.Reddit(client_id='CLIENT_ID',
+                             client_secret='CLIENT_SECRET',
+                             password='PASSWORD',
+                             user_agent='ModQ counter script by /u/zebradolphin5',
+                             username='USERNAME')
+        subreddit = reddit.subreddit('SUBREDDIT')
+        getCount(subreddit)
     except:
         error_message = traceback.format_exc()
-        now = str(datetime.datetime.now())
+        now = str(datetime.now())
         print(now + ' - Error:\n' + error_message + '\n\n\n')
         time.sleep(300)
+        setup()
 
+
+def getCount(subreddit):
+    while True:
+        try:
+            startTime = time.time()
+
+            print('Gathering items in queue...')
+            modQueue = subreddit.get_mod_queue()
+            count = 0
+
+            for item in modQueue:
+                count += 1
+            print('Total items in queue: ' + str(count))
+
+            if count >= 15:
+                alertTime = str(datetime.now())
+                print(alertTime + 'Items in queue higher than 15! Alerting Slack.')
+                message = '*Uhh..HELLO?? What are you doing?? THERE ARE {} THINGS IN MODQUEUE...GET TO WORK!* \n<http://www.reddit.com/r/mod/about/modqueue>'.format(str(count))
+                body = {'text': '<!here> *Q-KILLER* \n{}'.format(message), 'channel': '#general', 'username': 'BOT', 'icon_emoji': ':middle_finger:'}
+                r_headers = {'Content-type': 'application/json'}
+
+                try:
+                    r = requests.post('YOUR_WEBHOOK_URL', json=body, headers=r_headers)
+
+                    if r.status_code != 200:
+                        now = str(datetime.now())
+                        print(now + ' - Error:\n' + 'Received HTTP Response other than 200' + '\n\n\n')
+
+                except requests.exceptions.RequestException:
+                    req_error = traceback.format_exc()
+                    now = str(datetime.now())
+                    print(now + ' - Error:\n' + req_error + '\n\n\n')
+
+            elapsedTime = str(round(time.time() - startTime, 2))
+            finish = 'Cycle completed in {}s. \nWaiting for next interval...'.format(elapsedTime)
+            print(finish)
+            time.sleep(180)
+        except:
+            error_message = traceback.format_exc()
+            now = str(datetime.now())
+            print(now + ' - Error:\n' + error_message + '\n\n\n')
+            time.sleep(300)
+            setup()
+
+
+if __name__ == '__main__':
+    setup()
